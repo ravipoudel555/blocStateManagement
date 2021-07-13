276e79c801b59c2baef05fbc70cf2bb2290407a0
Ravi <ravipoudel555@gmail.com>
2021-07-13 14:50:10 +0545

    feat: add internet cubit

---
+++ lib/logic/cubit/internet_cubit.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:meta/meta.dart';
+
+import 'package:bloc_flutter/constants/enums.dart';
+
+part 'internet_state.dart';
+
+class InternetCubit extends Cubit<InternetState> {
+  final Connectivity connectivity;
+  late StreamSubscription connectivityStreamSubscription;
+
+  InternetCubit({required this.connectivity}) : super(InternetLoading()) {
+    connectivityStreamSubscription =
+        connectivity.onConnectivityChanged.listen((connectivityResult) {
+      if (connectivityResult == ConnectivityResult.wifi) {
+        emitInternetConnected(ConnectionType.Wifi);
+      } else if (connectivityResult == ConnectivityResult.mobile) {
+        emitInternetConnected(ConnectionType.Mobile);
+      } else if (connectivityResult == ConnectivityResult.none) {
+        emitInternetDisconnected();
+      }
+    });
+  }
+
+  void emitInternetConnected(ConnectionType _connectionType) =>
+      emit(InternetConnected(connectionType: _connectionType));
+
+  void emitInternetDisconnected() => emit(InternetDisonnected());
+
+  @override
+  Future<void> close() {
+    connectivityStreamSubscription.cancel();
+    return super.close();
+  }
+}
